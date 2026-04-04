199e3a42c9242b6444158b197a27ec96dd4de0aa
Ritika Mahato <ritikamahato221@gmail.com>
2026-04-04 18:30:27 +0530

    remove unuseful links from navbar

--- src/landing_page/Navbar.js
+++ src/landing_page/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import { FaGithub, FaLinkedin } from "react-icons/fa";
+
 function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg bg-dark border-bottom fixed-top">
